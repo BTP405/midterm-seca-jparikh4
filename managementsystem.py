5d5458fa2a284700f04cf4c410d7874a9b7d3f9d
jparikh4 <158526490+jparikh4@users.noreply.github.com>
2024-03-07 23:33:33 +0000

    commit

--- managementsystem.py
+++ managementsystem.py
@@ -86,7 +86,6 @@ class ManagementSystem:
         if emp_id not in self.employees:
         raise ValueError(f"Employee with ID {emp_id} not found.")
 
-       # Assign employee to project
         self.employees[emp_id].project_id = project_id
         self.projects[project_id].add_employee(emp_id)
         pass
